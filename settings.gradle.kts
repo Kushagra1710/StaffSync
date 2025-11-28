b951c5fbefb413324e5bef5043fe72ce738cd719
Kushagra1710 <118935536+Kushagra1710@users.noreply.github.com>
2025-11-29 03:15:41 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,39 @@
+rootProject.name = "EmployeeAttendanceApp"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+            url = uri("https://maven.google.com")  // Explicit Google Maven URL
+
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+include(":composeApp")
+include(":server")
+include(":shared")
